6643112932fd3690d7dca8751e46f19769d4f1a5
jiuhao <815514981@qq.com>
2026-09-08 13:00:33 +0800

    2026-09-08 13:00:34

--- .config/nvim/lua/plugin/alpha.lua
+++ .config/nvim/lua/plugin/alpha.lua
@@ -7,7 +7,7 @@ dashboard.section.header.opts.hl = "Normal"
 dashboard.section.buttons.val = {
 	dashboard.button("w", "Workspaces", ":Telescope workspaces<cr>"),
 	dashboard.button("r", "Recent", ":Telescope oldfiles<cr>"),
-	dashboard.button("s", "Settings", ":e $MYVIMRC | :cd %:p:h<cr>"),
+	dashboard.button("s", "Settings", ":e ~/Downloads/nixos/.config/nvim/init.lua | :cd %:p:h<cr>"),
 	dashboard.button("q", "Quit NVIM", ":qa<cr>"),
 }
 
