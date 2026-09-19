f033fec8fa1c46b0b5269f3bc2ed2703d6165140
jiuhao <815514981@qq.com>
2026-09-19 15:08:16 +0800

    2026-09-19 15:08:16

--- dotfile/.emacs.d/user-config/user-keybind.el
+++ dotfile/.emacs.d/user-config/user-keybind.el
@@ -56,14 +56,14 @@
 (defhydra hydra-org-table (:color blue :hint nil )
   "
 ☞ org-table
-[_c_]create         [_o_]export-table  [_v_]valign-toggle  [_d_]insert-row
-[_r_]insert-column  [_x_]del-column
+[_c_]create         [_o_]export-table  [_v_]valign-toggle  [_u_]insert-row
+[_l_]insert-column  [_x_]del-column
   "
   ("c"   org-table-create )
   ("o"   org-table-export-to-spreadsheet )
   ("v"   valign-mode )
-  ("r"   org-table-insert-row )
-  ("d"   org-table-insert-column )
+  ("u"   org-table-insert-row )
+  ("l"   org-table-insert-column )
   ("x"   org-table-delete-column )
   ("q"   keyboard-quit :color blue))
 
