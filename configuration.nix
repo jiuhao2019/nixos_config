1486155c9304a881c71a09887b601fcca5d6c017
jiuhao <815514981@qq.com>
2026-09-21 08:36:38 +0800

    2026-09-21 08:36:38

--- configuration.nix
+++ configuration.nix
@@ -27,8 +27,8 @@
 
   networking.hostName = "nixos"; 
   networking.networkmanager.enable = true;
-  networking.proxy.default = "http://127.0.0.1:7897";
-  networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+  # networking.proxy.default = "http://127.0.0.1:7897";
+  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
   networking.firewall.enable = false;
 
   time.timeZone = "Asia/Shanghai";
