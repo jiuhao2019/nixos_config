b9c528edb46648238b35188c08d71ff0e471f26a
jiuhao <815514981@qq.com>
2026-09-22 08:00:24 +0800

    2026-09-22 08:00:24

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -18,11 +18,12 @@ abbr ll "eza -al --git"
 abbr lt "eza -T"
 abbr ld "eza -lD"
 
-function gs
-    git add . && git commit -m (date '+%Y-%m-%d %H:%M:%S') && git push
+function gg
+    git add . && git commit -m (date '+%Y-%m-%d %H:%M:%S')
 end
 abbr gd "git diff"
 abbr gl "git pull"
+abbr gs "git push"
 
 abbr rm "rm -irv"
 
@@ -44,7 +45,7 @@ end
 
 # starship init fish | source
 zoxide init fish | source
-function zi
+function zz
     cd (zoxide query -i | string collect)
 end
 # 代理
