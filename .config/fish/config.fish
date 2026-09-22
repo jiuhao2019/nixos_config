be5cd535d6bbbb71ff6c893e24d2d6e28cffea66
jiuhao <815514981@qq.com>
2026-09-22 10:24:20 +0800

    2026-09-22 10:24:20

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -45,9 +45,10 @@ end
 
 # starship init fish | source
 zoxide init fish | source
-function zz
-    cd (zoxide query -i | string collect)
-end
+# function zz
+#     cd (zoxide query -i | string collect)
+# end
+alias zz zi
 # 代理
 set -x http_proxy http://127.0.0.1:7890
 set -x https_proxy http://127.0.0.1:7890
