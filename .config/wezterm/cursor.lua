84921ebae0b4faa35e6e8dfe1f4923a7f5c7cbf0
jiuhao <815514981@qq.com>
2026-09-22 12:46:05 +0800

    2026-09-22 12:46:05

--- .config/wezterm/cursor.lua
+++ .config/wezterm/cursor.lua
@@ -1,9 +1,9 @@
 return {
 	colors = {
 		-- 光标
-		cursor_fg = "#282828", -- 光标框中文字颜色
-		cursor_bg = "#ebdbb2", -- 光标块颜色
-		cursor_border = "#ebdbb2", -- 光标边框颜色
+		cursor_fg = "#000000", -- 光标框中文字颜色
+		cursor_bg = "#FABD2F", -- 光标块颜色
+		cursor_border = "#FABD2F", -- 光标边框颜色
 	},
 	-- Acceptable values are SteadyBlock, BlinkingBlock, SteadyUnderline, BlinkingUnderline, SteadyBar, and BlinkingBar.
 	default_cursor_style = "SteadyBlock",
