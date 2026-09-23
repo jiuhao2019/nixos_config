618c0ea4ac6c1574e200f7c068c78855aab72039
jiuhao <815514981@qq.com>
2026-09-23 09:43:50 +0800

    2026-09-23 09:43:50

--- dotfile/.emacs.d/init.el
+++ dotfile/.emacs.d/init.el
@@ -31,18 +31,4 @@
 
 
 (custom-set-variables)
-(custom-set-faces
- '(tab-bar
-   ((t (:background "#292929"
-        :foreground "#787878"
-        :box nil))))
- '(tab-bar-tab
-   ((t (:background "#6b83ac"
-        :foreground "#292929"
-        :weight bold
-        :box nil))))
- '(tab-bar-tab-inactive
-   ((t (:background "#3a3a3a"
-        :foreground "#787878"
-        :weight normal
-        :box nil)))))
+(custom-set-faces)
